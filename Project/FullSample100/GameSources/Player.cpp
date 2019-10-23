4d70c542ad0258532bfe4129a1c6ffa69bf533d9
Gameover555 <gameover555@ezweb.ne.jp>
2019-10-23 15:38:11 +0900

    プレイヤークラスを追加

--- Project/FullSample100/GameSources/Player.cpp
+++ Project/FullSample100/GameSources/Player.cpp
@@ -7,7 +7,23 @@
 #include "Project.h"
 
 namespace basecross{
+	//\‘¢‚Æ”jŠü
+	Player::Player(const shared_ptr<Stage>& ptrStage, const Vec3 pos, const Vec3 scale) :
+		GameObject(ptrStage),
+		m_pos(pos),
+		m_scale(scale)
+	{
+	}
 
+	//‰Šú‰»
+	void Player::OnCreate() {
+
+	}
+
+	//XV
+	void Player::OnUpdate() {
+
+	}
 }
 //end basecross
 
--- Project/FullSample100/GameSources/Player.h
+++ Project/FullSample100/GameSources/Player.h
@@ -5,10 +5,31 @@
 
 #pragma once
 #include "stdafx.h"
+#include"Project.h"
 
-namespace basecross{
+namespace basecross {
+	class Player : public GameObject
+	{
 
+	private:
 
+	protected:
+		Vec3 m_pos;//位置
+		Vec3 m_scale;//大きさ
+		Vec3 m_rot;//回転
+		float m_calory;//カロリー
+
+	public:
+		//構造と破棄
+		Player(const shared_ptr<Stage>& ptrStage, const Vec3 pos, const Vec3 scale);
+		~Player() {};
+
+		//初期化
+		virtual void OnCreate() override;
+		//更新
+		virtual void OnUpdate() override;
+	};
 }
+
 //end basecross
 
