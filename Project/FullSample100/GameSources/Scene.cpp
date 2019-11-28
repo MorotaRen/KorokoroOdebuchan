0ae31addd9bf3f3493cb0e65a8311f7914b0a8c6
MorotaRen <renwin25@gmail.com>
2019-11-28 13:32:21 +0900

    [Add]ファイル検索の下地

--- Project/FullSample100/GameSources/Scene.cpp
+++ Project/FullSample100/GameSources/Scene.cpp
@@ -56,6 +56,9 @@ namespace basecross {
 			App::GetApp()->RegisterTexture(texture.m_texKey, strTexture);
 		}
 	}
+	void LoadImageResources(wstring FolderName) {
+
+	}
 	/// ----------------------------------------------------------------------------<summary>
 	/// ‰æ‘œ‚Ì“Ç‚İ‚İ(ˆø”‚ ‚è)
 	/// </summary>----------------------------------------------------------------------------
