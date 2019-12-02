baae4184cc79fdfed8dc8a8b0e283d02fbe2dd45
吉田崇信 <zeal18300136@c-league.jp>
2019-12-02 11:13:24 +0900

    フェードイン、フェードアウトの追加

--- Project/FullSample100/GameSources/Fade.cpp
+++ Project/FullSample100/GameSources/Fade.cpp
@@ -13,11 +13,53 @@ namespace basecross {
 	//--------------------------------------------------------------------------------------
 	FadeSprite::FadeSprite(const shared_ptr<Stage>&stagePtr,
 		const FadeType& fadeType,
-		const wstring&stageString):
-		Sprite(stagePtr,L"Black",Vec2(1280.0f,800.0f),Vec2(0.0f,0.0f)),
+		const wstring&stageString) :
+		Sprite(stagePtr, L"Black", Vec2(1280.0f, 800.0f), Vec2(0.0f, 0.0f)),
 		m_Type(fadeType),
 		m_StageString(stageString)
 	{}
+
+	void FadeSprite::OnCreate() {
+		Sprite::OnCreate();
+		SetDrawLayer(150);
+		auto ptrDraw = GetComponent<PCTSpriteDraw>();
+		switch (m_Type)
+		{
+		case FadeIn:
+			ptrDraw->SetDiffuse(Col4(1.0f, 1.0f, 1.0f, 1.0f));
+			break;
+		case FadeOut:
+			ptrDraw->SetDiffuse(Col4(1.0f, 1.0f, 1.0, 0.0f));
+			break;
+		default:
+			break;
+		}
+	}
+	void FadeSprite::OnUpdate() {
+		
+		auto ptrDraw = GetComponent<PCTSpriteDraw>();
+		Col4 color = ptrDraw->GetDiffuse();
+		switch (m_Type)
+		{
+		case FadeIn:
+			color.w -= m_FadeSpeed;
+			ptrDraw->SetDiffuse(color);
+			if (color.w < 0.0f) {
+				GetStage()->RemoveGameObject<GameObject>(GetThis<GameObject>());
+			}
+			break;
+		case FadeOut:
+			color.w += m_FadeSpeed;
+			ptrDraw->SetDiffuse(color);
+			if (color.w > 1.0f) {
+				GetStage()->PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), m_StageString);
+				GetStage()->RemoveGameObject<GameObject>(GetThis<GameObject>());
+			}
+			break;
+		default:
+			break;
+		}
+	}
 
 }
 
