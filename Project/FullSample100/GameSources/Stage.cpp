d2507fcd1e17a685944a23d4833654e11212fedc
MorotaRen <renwin25@gmail.com>
2019-12-04 11:51:49 +0900

    [Add]ステージ入れました

--- Project/FullSample100/GameSources/Stage.cpp
+++ Project/FullSample100/GameSources/Stage.cpp
@@ -25,7 +25,7 @@ namespace basecross {
 	/// </summary>----------------------------------------
 	void StageObject::OnCreate() {
 		auto TransComp = GetComponent<Transform>();
-		m_pos.x += -0.1f;
+		//m_pos.x += 0.05f;
 		m_pos.z += -0.0f;
 		TransComp->SetPosition(m_pos);
 		//‘å‚«‚¢‚Ì‚Å’²®
@@ -34,7 +34,7 @@ namespace basecross {
 		m_quat.rotationY(3.14);
 		TransComp->SetQuaternion(m_quat);
 
-		auto DrawComp = AddComponent<BcPNTStaticDraw>();
+		auto DrawComp = AddComponent<BcPNTStaticModelDraw>();
 		DrawComp->SetMultiMeshResource(L"M_Spring");
 	}
 }
